b9556bd143c47a654ea235adc58b45f5d2234bc8
Manas-Ram <68700139+Manas-Ram@users.noreply.github.com>
2025-02-27 04:36:41 -0500

    added OAuthv2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -68,7 +68,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/auth/google/callback",
+            callbackURL: "http://localhost:3001/auth/google/callback",
         },
         (accessToken, refreshToken, profile, done) => {
             // Extract email and name from Google profile
